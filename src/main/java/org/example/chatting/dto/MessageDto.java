0297e27ec2ee2c535a8466ecea0e36c84ae3d3a6
aaahyunseo <hs030608@naver.com>
2025-01-22 00:56:30 +0900

    MessageDto 추가

---
+++ src/main/java/org/example/chatting/dto/MessageDto.java
@@ -0,0 +1,15 @@
+package org.example.chatting.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class MessageDto {
+    private String userId;
+    private String content;
+}
